f10c42e54002ca8ce12e3c31a7ba98b08af30bf0
LSmyrnaios <lsmyrnaios@gmail.com>
2018-10-24 17:49:14 -0700

    Rename "relation"-vars in "main.c".

--- main.c
+++ main.c
@@ -17,26 +17,26 @@ int main(void) {
     /* So we are going to use mod(%2^n) to get the last n bits, where 2^n is also the number of buckets */
     int32_t number_of_buckets = (int32_t) pow(2, n);
 
-    Relation *relation1, *relation2;
+    Relation *relationR, *relationS;
 
     /* Initialize the relations with random numbers from 0-200 */
     int initReturnCode = 0;
-    if ( (initReturnCode = initializeRelations(&relation1, &relation2, number_of_buckets, 10, 5, 1)) == -1 ) {
+    if ( (initReturnCode = initializeRelations(&relationR, &relationS, number_of_buckets, 10, 5, 1)) == -1 ) {
         return initReturnCode;  // ErrorCode
     }
 
     /* Do Radix Hash Join on the conjunction of the relations*/
-    Result *result = RadixHashJoin(relation1, relation2, number_of_buckets);
+    Result *result = RadixHashJoin(relationR, relationS, number_of_buckets);
     /*if(result == NULL){
         printf("Malloc failed!\n");
         perror("Malloc");
         return -1;
     }*/
 
-    free(relation1->tuples);
-    free(relation2->tuples);
+    free(relationR->tuples);
+    free(relationS->tuples);
 
-    free(relation1);
-    free(relation2);
+    free(relationR);
+    free(relationS);
 }
 
