0dcc267e53abc1e3d62167769959f2451f23d5d8
LSmyrnaios <lsmyrnaios@gmail.com>
2018-11-28 19:07:21 +0200

    Update UnitTesting dependencies.

--- scripts/installUnitTestingDependencies.sh
+++ scripts/installUnitTestingDependencies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+sudo apt-get install -y g++
+sudo apt-get install -y g++-multilib
+sudo apt-get install -y libc6-dev-i386
 sudo apt-get install -y ruby
 sudo apt-get install -y rake
-sudo apt-get install -y libc6-dev-i386
-sudo apt-get install -y g++-multilib
