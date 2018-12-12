50790127dc9ee83306f5da16fa97e474f647c46c
LSmyrnaios <lsmyrnaios@gmail.com>
2018-12-12 23:23:18 +0200

    Cleanup "#includes".

--- src/file_io.h
+++ src/file_io.h
@@ -1,13 +1,12 @@
 #ifndef ADVANCED_DBMS_IMPLEMENTATION_FILE_IO_H
 #define ADVANCED_DBMS_IMPLEMENTATION_FILE_IO_H
 
-#include "supportFunctions.h"
-#include <inttypes.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <errno.h>
+
+#include "supportFunctions.h"
 
 Table **read_tables(char* base_path, char* init_filename, int *num_of_tables, uint64_t ***mapped_tables, int **mapped_tables_sizes);
 
